cce9baa7f48ac735b34c84e09b3b56a1482a12cb
sunishae <sunishae44@gmail.com>
2025-10-27 05:06:18 +0900

    refactor: Application 완성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,31 @@
 package racingcar;
 
+import racingcar.controller.RaceController;
+import racingcar.domain.ParseCar;
+import racingcar.domain.RaceGame;
+import racingcar.domain.RoundValidator;
+import racingcar.domain.WinnerCheck;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        RoundValidator roundValidator = new RoundValidator();
+        InputView inputView = new InputView(roundValidator);
+        OutputView outputView = new OutputView();
+        ParseCar parseCar = new ParseCar();
+        RaceGame raceGame = new RaceGame();
+        WinnerCheck winnerCheck = new WinnerCheck();
+
+        RaceController raceController = new RaceController(
+                inputView,
+                outputView,
+                parseCar,
+                raceGame,
+                winnerCheck
+        );
+
+        raceController.run();
     }
 }
