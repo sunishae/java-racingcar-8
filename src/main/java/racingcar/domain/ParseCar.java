fc9242bd182865d8e82b710f5a03182fbef13cf5
sunishae <sunishae44@gmail.com>
2025-10-27 04:03:19 +0900

    refactor: Error메세지 상수 이름 변경

--- src/main/java/racingcar/domain/ParseCar.java
+++ src/main/java/racingcar/domain/ParseCar.java
@@ -7,7 +7,7 @@ public class ParseCar {
 
     private static final int BASE_POSITION = 0;
     private static final int MAX_NAME_LENGTH = 5;
-    private static final String EXCEPTION_MESSAGE = "[ERROR] 자동차 이름은 1자 이상 5자 이하여야 합니다.";
+    private static final String ERROR_INVALID_CAR_NAME = "[ERROR] 자동차 이름은 1자 이상 5자 이하여야 합니다.";
 
 
     public List<Car> parse(String inputString) {
@@ -25,7 +25,7 @@ public class ParseCar {
 
     private void validateName(String name) {
         if (name.isEmpty() || name.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException(EXCEPTION_MESSAGE);
+            throw new IllegalArgumentException(ERROR_INVALID_CAR_NAME);
         }
     }
 }
